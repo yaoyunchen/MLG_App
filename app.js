544f9cf159e545adbc49811ebf80730184df2b20
Andy Kwan <zelthrox@gmail.com>
2016-04-30 13:09:03 -0700

    setting up heroku psql

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//heroku postgresql
+var pg = require('pg');
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// })
+
 // Express Instance
 var app = express();
 
