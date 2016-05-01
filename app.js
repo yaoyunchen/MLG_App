097f7156f6849ebf9eaea6219730129764dfa15f
Andy Kwan <zelthrox@gmail.com>
2016-05-01 15:33:39 -0700

    clean old db codes

--- app.js
+++ app.js
@@ -10,19 +10,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//heroku postgresql
-// app.get('/db', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//     client.query('SELECT * FROM test_table', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// })
-
 // Express Instance
 var app = express();
 
