87e75889a9ecfdfa0e6a45edd474d5e899d1538b
Andy Kwan <zelthrox@gmail.com>
2016-05-09 12:26:06 -0700

    added interval match update

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var bodyParser = require('body-parser');
 // Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var matches = require('./routes/matches');
-
+var matches = require('/routes/matches');
 // Express Instance
 var app = express();
 
@@ -66,24 +65,24 @@ app.use(function(err, req, res, next) {
 ///////////////////////////////////////
 //setting up period database checking//
 ///////////////////////////////////////
-//providing database data
+//providing database dependencies
 var http = require('http');
 var pg = require('pg');
 var dbKey = require('./env.js');
+var request = require('request');
 var conString = dbKey();
-//for debugging only
+var API_KEY = '';
+if (process.env.NODE_ENV != 'production') {
+  var env = require('./env.js');
+  API_KEY = process.env.LOL_API_KEY;
+} 
+//for Debugging only
 var counter = 0;
-setInterval(function() {
-  //for debugging only
-  counter ++;
-  console.log('the counter is: ' + counter);
 
-  //code
-  var username = counter;
-  var champion = 1;
-  var bet = 1;
-  var betType = 1;
-  var matchType = 1;
+//Updating MatchRequest Table
+setInterval(function() {
+  counter ++;
+  //Expire Match Requests past 15minutes
   pg.connect(conString, function(err, client, done) {
     if(err) {
       return console.error('error fetching client from pool', err);
@@ -105,6 +104,7 @@ setInterval(function() {
       AND matchrequests.status IN (1,2)
       AND users.id = matchrequests.user_id),0);`,
     function(err, result) {
+      done();
       if(err) {
         return console.error('error running query', err);
       }
@@ -121,11 +121,291 @@ setInterval(function() {
         console.log("Expired Match Requests Older Than 15 Minutes" + counter);
       });    
     });
-
-
   });
+}, 60000); // every 60 seconds
 
-}, 600000); // every 5 seconds
+//Updating MatchRequest Table
+setInterval(function() {
+  // (1) updating for ending matches
+  // (2) updating for non-ended Matches
+  function updateMatchDB(match,client,done,userNewLastGameId,opponentNewLastGameId,userGamesPlayed,opponentGamesPlayed){
+    client.query(
+      "UPDATE Matches \
+      SET user_total_games_played = (user_total_games_played + " + userGamesPlayed +"), \
+      user_last_game_id = " + userNewLastGameId + ", \
+      opponent_total_games_played = (opponent_total_games_played + " + opponentGamesPlayed + "), \
+      opponent_last_game_id = " + opponentNewLastGameId + ", \
+      status = 3, \
+      update_time = CURRENT_TIMESTAMP \
+      WHERE end_time <= CURRENT_TIMESTAMP \
+      AND status = 2 \
+      AND id = " + match.id + "; \
+      \
+      UPDATE Matches \
+      SET user_total_games_played = (user_total_games_played + " + userGamesPlayed +"), \
+      user_last_game_id = " + userNewLastGameId + ", \
+      opponent_total_games_played = (opponent_total_games_played + " + opponentGamesPlayed + "), \
+      opponent_last_game_id = " + opponentNewLastGameId + ", \
+      update_time = CURRENT_TIMESTAMP \
+      WHERE end_time > CURRENT_TIMESTAMP \
+      AND status = 2 \
+      AND id = " + match.id + ";"
+      ,function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      //do stuff here
+      console.log("DONE GAMES ENDED FOR MATCH: #" + match.id);
+    });
+  }
+
+  //API call for 10 recent opponent games
+  function getOppData(match,client,done,userNewLastGameId,userOldGameReached,userGamesPlayed){
+    var match = match;
+    var path = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + match.opponent_summoner_id + '/recent?api_key=' + API_KEY;
+    request.get(path, function(err, response) {
+      if (!err) {
+        var oppJson = JSON.parse(response.body);
+        var opponentGamesPlayed = 0;
+        var opponentOldGameReached = false;
+        var opponentNewLastGameId = 0;
+        for (var oppKey in oppJson){
+          if (!oppJson.hasOwnProperty(oppKey)) continue;
+          var oppGame = oppJson[oppKey];
+          for(var oppAttr in oppGame){
+            if(!oppGame.hasOwnProperty(oppAttr)) continue;
+            //set new value for last_game_played
+            if (opponentNewLastGameId === 0) {
+              opponentNewLastGameId = oppGame[0].gameId;
+            }
+            //set opponentOldGameReached to true if gameId is the same as the last_game_played
+            if(oppGame[oppAttr].gameId === match.opponent_last_game_id){
+              opponentOldGameReached = true;
+            } 
+            //update game count if game isnt old and champion is the same
+            else if(oppGame[oppAttr].championId === match.champion_id){
+              opponentGamesPlayed ++;
+            }
+          }   
+        }
+
+        updateMatchDB(match,client,done,userNewLastGameId,opponentNewLastGameId,userGamesPlayed,opponentGamesPlayed);
+      } else {
+        console.error(err);
+      }
+    });
+  }
+
+  //API call for 10 recent user games
+  function getUserData(match,client,done){
+    var match = match;
+    var path = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + match.user_summoner_id + '/recent?api_key=' + API_KEY;
+    request.get(path, function(err, response) {
+      if (!err) {
+        var userJson = JSON.parse(response.body);
+        var userGamesPlayed = 0;
+        var userOldGameReached = false;
+        var userNewLastGameId = 0;
+        for (var userKey in userJson){
+          if (!userJson.hasOwnProperty(userKey)) continue;
+          var userGame = userJson[userKey];
+          for(var userAttr in userGame){
+            if(!userGame.hasOwnProperty(userAttr)) continue;
+
+            //set new value for last_game_played
+            if (userNewLastGameId === 0) {
+              userNewLastGameId = userGame[0].gameId;
+            }
+            //set userOldGameReached to true if gameId is the same as the last_game_played
+            if(userGame[userAttr].gameId === match.user_last_game_id){
+              userOldGameReached = true;
+            } 
+            //update game count if game isnt old and champion is the same
+            else if(userGame[userAttr].championId === match.champion_id){
+              userGamesPlayed ++;
+            }
+          }   
+        }
+
+        getOppData(match,client,done,userNewLastGameId,userOldGameReached,userGamesPlayed);
+      } else {
+        console.error(err);
+      }
+    });
+  }
+
+  //Return Games that are within the 3 hours interval
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(
+      `SELECT m.id, m.user_id user_id, u1.summoner_id user_summoner_id, r.user_id as opponent_id, u2.summoner_id as opponent_summoner_id, r.champion_id, user_last_game_id, opponent_last_game_id, (m.end_time - CURRENT_TIMESTAMP) time_left
+      FROM matches m
+      JOIN matchrequests r ON m.id = r.match_id
+      JOIN users u1 ON m.user_id = u1.id
+      JOIN users u2 ON r.user_id = u2.id
+      Where m.status = 2
+      AND m.user_id != r.user_id
+      AND m.update_time <= CURRENT_TIMESTAMP - (180 * INTERVAL '1 MINUTE');`,
+    function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      //got results form select, calling API for data to use for update
+      for (var key in result.rows){
+        if (!result.rows.hasOwnProperty(key)) continue;
+        var match = result.rows[key];
+        // Recent 10 matches data
+        getUserData(match,client,done);
+      }   
+    });
+  });
+}, 60000); // every 60 seconds
+
+
+//Updating MatchRequest Table
+setInterval(function() {
+
+  //updates DB
+  function updataEndedMatches(match,client,done,userJson,oppJson){
+    //need to be placed into Bet Type TAble
+    var winnerPayout=0;
+    var loserPayout=0;
+    if(match.bettype == 0){
+      winnerPayout = 1.5*match.bet;
+      loserPayout = 0.5*match.bet;
+    } else if(match.bettype == 1){
+      winnerPayout = 2*match.bet;
+      loserPayout = 0*match.bet;
+    }
+
+    //checks winner
+    var userMPointsGained = userJson.championPoints - match.user_points;
+    var oppMPointsGained = oppJson.championPoints - match.opponent_points;
+    var userPayout=0;
+    var oppPayout=0;
+    if(userMPointsGained >= oppMPointsGained){
+      userPayout = winnerPayout;
+      oppPayout = loserPayout;
+    } else if(userMPointsGained < oppMPointsGained){
+      userPayout = loserPayout;
+      oppPayout = winnerPayout;
+    }
+
+    client.query(`
+      UPDATE Matches
+      SET user_points =`+userJson.championPoints+`,
+          opponent_points =`+oppJson.championPoints+`,
+          status = 4,
+          update_time = CURRENT_TIMESTAMP
+      WHERE status = 3
+      AND id = `+match.id+`;
+
+      UPDATE Users
+      SET mlg_points = mlg_points +`+userPayout+`,
+          update_time = CURRENT_TIMESTAMP
+      WHERE id =`+match.user_id+`;
+
+      UPDATE Users
+      SET mlg_points = mlg_points +`+oppPayout+`,
+          update_time = CURRENT_TIMESTAMP
+      WHERE id =`+match.opponent_id+`;
+      `,function(err, result) {
+        done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      //do stuff here
+      console.log("DONE PAYING OUT MATCH: #" + match.id);
+    });
+  }
+
+  //Get Opponent Mastery Data
+  function getOppChampMastery(match,client,done,userJson){
+    var match = match;
+    var path = 'https://na.api.pvp.net/championmastery/location/NA1/player/' + match.opponent_summoner_id + '/champion/' + match.champion_id + '?api_key=' + API_KEY;
+    request.get(path, function(err, response) {
+    done();
+    if (!err) {
+      if (response.body == ""){
+        response.body = `{
+          "playerId": `+match.opponent_summoner_id+`,
+          "championId": `+match.champion_id+`,
+          "championLevel": 0,
+          "championPoints": 0,
+          "lastPlayTime": 0,
+          "championPointsSinceLastLevel": 0,
+          "championPointsUntilNextLevel": 0,
+          "chestGranted": false          
+        }`;
+      }
+      var oppJson = JSON.parse(response.body);
+      //do stuff
+      updataEndedMatches(match,client,done,userJson,oppJson);
+
+      }
+    });
+  }
+
+  //Get User Mastery Data
+  function getUserChampMastery(match,client,done){
+    var match = match;
+    var path = 'https://na.api.pvp.net/championmastery/location/NA1/player/' + match.user_summoner_id + '/champion/' + match.champion_id + '?api_key=' + API_KEY;
+    request.get(path, function(err, response) {
+      done();
+      if (!err) {
+        if (response.body == ""){
+          response.body = `{
+            "playerId": `+match.user_summoner_id+`,
+            "championId": `+match.champion_id+`,
+            "championLevel": 0,
+            "championPoints": 0,
+            "lastPlayTime": 0,
+            "championPointsSinceLastLevel": 0,
+            "championPointsUntilNextLevel": 0,
+            "chestGranted": false          
+          }`;
+      }
+      var userJson = JSON.parse(response.body);
+
+      getOppChampMastery(match,client,done,userJson);
+      } else {
+        console.error(err);
+      }
+    });
+  }
+
+  //Payout Matches that are over
+  pg.connect(conString, function(err,client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(
+      `SELECT m.id, m.user_id user_id, u1.summoner_id user_summoner_id, m.user_points, r.user_id as opponent_id, u2.summoner_id as opponent_summoner_id, m.opponent_points, r.champion_id, r.bet, r.bettype
+      FROM matches m
+      JOIN matchrequests r ON m.id = r.match_id
+      JOIN users u1 ON m.user_id = u1.id
+      JOIN users u2 ON r.user_id = u2.id
+      WHERE m.status = 3
+      AND m.user_id != r.user_id;`,
+      function(err, result) {
+        done();
+        if(err) {
+          return console.error('error running query', err);
+        }
+        //got results form select, calling API for data to use for update
+        for (var key in result.rows){
+          if (!result.rows.hasOwnProperty(key)) continue;
+          var match = result.rows[key];
+          // Get Matery Points for user
+          getUserChampMastery(match,client,done);
+        }   
+      });
+    });
+  }, 60000); // every 60 seconds
 
 ///////////////////////////////////////
             //END HERE//
